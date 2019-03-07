90228f7a28a8d91a40dea554bc6cba4fc889cae9
Akshay Rathod <akshayrathod9@gmail.com>
2019-03-07 17:40:21 +0530

    Python Flask From Scratch [Part - 1] - Traversy Media

---
+++ templates/data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            "id" : 1,
+            "title" : "Article One",
+            "body" : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt recusandae quasi at, cum delectus adipisci iure suscipit eveniet quam cupiditate in mollitia id eaque provident omnis impedit laboriosam placeat minima.",
+            "author" : "Akshay Rathod",
+            "create_date" : "07-03-2019"
+        },
+        {
+            "id" : 2,
+            "title" : "Article Two",
+            "body" : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt recusandae quasi at, cum delectus adipisci iure suscipit eveniet quam cupiditate in mollitia id eaque provident omnis impedit laboriosam placeat minima.",
+            "author" : "Pratik Rathod",
+            "create_date" : "07-03-2019"
+        },
+        {
+            "id" : 3,
+            "title" : "Article Three",
+            "body" : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt recusandae quasi at, cum delectus adipisci iure suscipit eveniet quam cupiditate in mollitia id eaque provident omnis impedit laboriosam placeat minima.",
+            "author" : "Akshay Rathod",
+            "create_date" : "07-03-2019"
+        }
+    ]
+
+    return articles
